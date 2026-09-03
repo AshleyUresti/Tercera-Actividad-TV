204690b3237ea4b6289a15ee266c2ae95ed0e351
Ashley Janeth Uresti Martinez <zS25025286@estudiantes.uv.mx>
2026-09-03 13:38:32 -0600

    actualizar el Main.java
    Updated the Main class to include TV functionality and methods.

--- Main.java
+++ Main.java
@@ -1,69 +1,18 @@
-public class TV {
-    //atributos
-    private int channel;
-    private int volumeLevel;
-    private boolean on;
+public class Main {
+    public static void main(String[] args) {
+        TV tv = new TV(1, 50, false);
 
-    //constructor
-    public TV(int channel, int volumeLevel, boolean status) {
-        this.channel = channel;
-        this.volumeLevel = volumeLevel;
-        this.on = status;
+        System.out.println("Estado de la TV");
+        System.out.println("canal: 1\nvolumen: 50\nestado: apagada\n");
+
+        tv.turnOn();
+        tv.setChannel(5);
+        tv.channelUp();
+        tv.channelDown();
+        tv.volumeUp();
+        tv.volumenDown();
+        tv.volumenDown();
+        tv.turnOf();
+
     }
-
-    //metodos
-    public void turnOn() {
-        this.on = true;
-        System.out.println("TV encendida");
-    }
-
-    public void turnOf() {
-        this.on = false;
-        System.out.println("TV apagada");
-    }
-
-    public void setChannel(int newChannel) {
-        if (this.on == true) {
-            this.channel = newChannel;
-            System.out.println("El programa de TV ha cambiado a " + newChannel);
-        } else {
-            System.out.println("La TV está apagada");
-        }
-    }
-
-    public void channelUp() {
-        this.channel = this.channel + 1;
-        System.out.println("Se ha cambiado al canal " + this.channel);
-    }
-
-    public void channelDown() {
-
-        if (this.channel != 0) {
-            this.channel = this.channel - 1;
-            System.out.println("Se ha cambiado al canal " + this.channel);
-        } else {
-            System.out.println("El canal actual es 0");
-        }
-    }
-
-    public void volumeUp() {
-        if (this.volumeLevel >= 0 && this.volumeLevel < 100) {
-            this.volumeLevel = this.volumeLevel + 1;
-            System.out.println("El volumen subió a " + this.volumeLevel);
-        }
-        if (this.volumeLevel == 100 ) {
-            System.out.println("Volumen máximo 100");
-        }
-    }
-
-    public void volumenDown() {
-        if (this.volumeLevel > 0 && this.volumeLevel <= 100) {
-            this.volumeLevel = volumeLevel - 1;
-            System.out.println("El volumen bajó a " + this.volumeLevel);
-        }
-        if (this.volumeLevel == 0) {
-            System.out.println("El volumen está al mínimo 0");
-        }
-    }
-
 }
